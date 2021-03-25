c85bc774dca2db44393a70a52afebf641738f540
Gustavo Mendes <gusm217@gmail.com>
2021-03-25 15:06:44 -0300

    Adiciona exercicios 4 a 6

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -2,6 +2,12 @@ let a;
 let b;
 let c;
 
+let lado1;
+let lado2;
+let lado3;
+let somaLados = lado1 + lado2 + lado3;
+
+
 // 1
 let adicao = a + b;
 let subtracao = a - b;
@@ -29,4 +35,37 @@ else {
 }
 
 // 4
+if (a >= 0) {
+    console.log("Positivo")
+}
+else {
+    console.log("Negativo")
+}
+
+// 5
+
+if (somaLados ===  180){
+    console.log(true)
+}
+else {
+    console.log("Lados não positvos ou ultrapassam os limites de um triângulo")
+
+}
+
+
+//  6
+let xadrez = Cavalo.toLowerCase()
+
+switch (xadrez) {
+    case "rei":
+    console.log("O rei pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.");
+    break;
+    case "cavalo":
+    console.log("É a única peça que pode pular as outras. O movimento do cavalo é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.");
+    break;
+    case "rainha":
+    console.log("A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.");
+    break;
+
+}
 
