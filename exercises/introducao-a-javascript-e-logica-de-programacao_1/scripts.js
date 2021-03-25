360cec942680cf3d37b7bf94d898a016ff6f8aca
Gustavo Mendes <gusm217@gmail.com>
2021-03-25 13:46:13 -0300

    Adiciona primeiro exercicio de 1 a 3

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -0,0 +1,32 @@
+let a;
+let b;
+let c;
+
+// 1
+let adicao = a + b;
+let subtracao = a - b;
+let  multiplicacao = a * b;
+let  divisao = a / b;
+let  modulo = a % b;
+
+// 2
+if (a > b) {
+    console.log(a)
+}
+else {
+    console.log(b)
+}
+
+// 3
+if (a > b && a > c) {
+    console.log(a)
+}
+else if (b > a && b > c) {
+    console.log(b)
+}
+else {
+    console.log(c)
+}
+
+// 4
+
