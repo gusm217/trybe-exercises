c854093f63176556b1437716951222a2c76714cd
Gustavo Mendes <gusm217@gmail.com>
2021-03-25 21:40:54 -0300

    Adiciona exercicio 2 de JS ate numero 7

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/script.js
@@ -0,0 +1,64 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// 1
+for (let i = 0; i < numbers.length; i+=1) {
+    console.log(numbers[i])
+}
+
+// 2
+let  soma = 0
+for (let i = 0; i < numbers.length; i+=1) {
+    soma = soma + numbers[i]    
+}
+console.log(soma)
+
+// 3
+let mediaAri;
+for (let i = 0; i < numbers.length; i+=1) {
+    soma = soma + numbers[i];
+    if(i ==  9) {
+        mediaAri = soma / 10
+    }    
+}
+console.log(mediaAri);
+
+// 4
+if (mediaAri >= 20) {
+    console.log("valor maior que 20")
+}
+else {
+    console.log("valor menor ou igual a 20")
+    
+}
+
+// 5
+let maiorValor = 0
+for (let i = 0; i < numbers.length; i+=1) {
+    if (maiorValor < numbers[i]) {
+        maiorValor = numbers[i]
+    }
+}
+console.log(maiorValor)
+
+// 6
+let impares = 0
+for (let i = 0; i < numbers.length; i+=1) {
+    if (numbers[i] % 2 != 0) {
+        impares += 1
+    } else if (impares == 0){
+        console.log('Nenhum valor impar encontrado')
+    }
+
+}
+console.log(impares)
+
+// 7
+let  menorValor = 200;
+
+for (let i = 0; i < numbers.length; i+=1) {
+    if (numbers[i] < menorValor) {
+        menorValor = numbers[i]
+    }
+}
+console.log(menorValor)
+
