8867d7015bd1f318b89d362275b45c96bb4949f6
Gustavo <gusm217@gmail.com>
2021-03-31 19:44:39 -0300

    Adiciona os exercicios de 1 a 4 do dia 4.3

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/script.js
@@ -0,0 +1,57 @@
+// Exercício 1
+// n = 5
+
+// *****
+// *****
+// *****
+// *****
+// *****
+let n = 5;
+
+for (let index = 1; index <= n; index += 1) {
+    console.log("*".repeat(n))        
+}
+
+// Exercício 2
+// n = 5
+
+// *
+// **
+// ***
+// ****
+// *****
+
+for (let index = 1; index <= n; index += 1) {
+    console.log('*'.repeat(index))
+}
+
+
+// Exercício 3
+// n = 5
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+let blank;
+for (let index = 1; index <= n; index += 1) {
+    blank = n - index
+    console.log(' '.repeat(blank) + '*'.repeat(index))
+}
+
+// Exercício 4
+// n = 5
+
+//   *
+//  ***
+// *****
+
+for (let index = 1; index <= n; index += 1){
+    let emBranco = Math.ceil(n-index/2);
+    if (index % 2 != 0) {
+        console.log(' '.repeat(emBranco) + '*'.repeat(index) + ' '.repeat(emBranco))
+    } else {
+        console.log('\n')
+    }
+}
