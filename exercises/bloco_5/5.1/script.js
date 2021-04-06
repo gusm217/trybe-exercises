bba2575c537a1e6953707e87e1f9b2e696b1794c
Gustavo Mendes <gusm217@gmail.com>
2021-04-05 21:24:57 -0300

    Exercicio 5.1

---
+++ exercises/bloco_5/5.1/script.js
@@ -0,0 +1,7 @@
+document.getElementById('header-container').style.backgroundColor = 'green';
+document.querySelector('.emergency-tasks').style.backgroundColor = 'pink';
+document.querySelectorAll('.emergency-tasks div h3').style.backgroundColor = 'red';
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow';
+document.querySelector('.no-emergency-tasks div h3').style.backgroundColor = 'black'
+document.getElementById('footer-container').style.backgroundColor = '#000000';
+document.querySelectorAll('footer div').innerText = 'Administrador de tempo finalizado.';
