b9b4fc0472096b8a8c622f0b31e5e43a7d80b3fb
Gustavo Mendes <gusm217@gmail.com>
2021-04-08 20:37:28 -0300

    4.4 parte 2 - ok menos 1

--- exercises/bloco_4/4.4/introducao-a-javascript-e-logica-de-programacao_4/script.js
+++ exercises/bloco_4/4.4/introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -1,3 +1,5 @@
+// ------------ PARTE 1 ------------
+
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
@@ -47,3 +49,64 @@ for (let index in info) {
     }
 
 }
+
+
+// ------------- PARTE 2 --------------
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+// NÃO CONSEGUI =/
+
+function palindrome(word) {    
+    for (let index = 0; index < word.length; index += 1) {
+        let position = position - (word.length - 1);
+        if (index === position) {
+            return  true
+        }
+    }
+    return false;
+}
+
+console.log(palindrome('arara'));
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete. NÃO CONEGUI =/
+
+function aparecido(array) {
+    let objectCounter = []
+    let count = 0;
+    for (let index in array) {        
+        for (let j in array) {
+            if (array[index] === array[j]) {
+                count += 1;
+            }
+            object = {
+                index: count
+            }
+            objectCounter.push(object);
+        }
+    }
+    return objectCounter;
+}
+
+console.log(aparecido([5, 2, 5, 2, 10, 8, 8, 8, 7]));
+
+// 6 - Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
+
+function itSelf(number) {
+    let sum = 0;
+    for (let index = 1; index <= number; index += 1) {
+        sum += index;
+    }
+    return sum;
+}
+
+console.log(itSelf(7));
+
+// 7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
+
+function verifyWord(word, ending) {
+    if (word.length > ending.length) {
+        return true;
+    }
+    return false;
+}
+
+console.log(verifyWord('trybe', 'lasjdfjasdf'));
