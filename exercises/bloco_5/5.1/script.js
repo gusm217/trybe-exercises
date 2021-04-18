f11ccc42967363eea9e838ea1cb7cf5887bbb1fe
Gustavo <gusm217@gmail.com>
2021-04-18 17:44:44 -0300

    Finalizado o exercicio 5.1

--- exercises/bloco_5/5.1/script.js
+++ exercises/bloco_5/5.1/script.js
@@ -1,7 +1,20 @@
+let emergencyTasks = document.querySelector('.emergency-tasks');
+let notEmergentTasks = document.querySelector('.no-emergency-tasks');
+let notEmergent = document.querySelector('.no-emergency-tasks');
+let emergencyTitles = document.querySelectorAll('.emergency-titles');
+let notEmergentTitles = document.querySelectorAll('.not-emergent-titles');
+let footer = document.getElementById('footer-container');
+
 document.getElementById('header-container').style.backgroundColor = 'green';
-document.querySelector('.emergency-tasks').style.backgroundColor = 'pink';
-document.querySelectorAll('.emergency-tasks div h3').style.backgroundColor = 'red';
-document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow';
-document.querySelector('.no-emergency-tasks div h3').style.backgroundColor = 'black'
-document.getElementById('footer-container').style.backgroundColor = '#000000';
-document.querySelectorAll('footer div').innerText = 'Administrador de tempo finalizado.';
+
+emergencyTasks.style.backgroundColor = '#FF9F84';
+for (let index = 0; index < emergencyTitles.length; index += 1 ) {
+    emergencyTitles[index].style.backgroundColor = '#A500F3';
+}
+
+notEmergent.style.backgroundColor = '#F9DB5E';
+for (let index = 0; index < notEmergentTitles.length; index += 1) {
+    notEmergentTitles[index].style.backgroundColor = 'black';  
+}
+
+footer.style.backgroundColor = '#003533';
