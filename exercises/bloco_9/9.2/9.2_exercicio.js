3ee5644db3fd9b8ce76780bd235dc850956054e2
Gustavo Mendes <gusm217@gmail.com>
2021-05-13 10:25:39 -0300

    Adiciona o 2

--- exercises/bloco_9/9.2/9.2_exercicio.js
+++ exercises/bloco_9/9.2/9.2_exercicio.js
@@ -1,17 +1,44 @@
 // const fetch = require('node-fetch');
 // const { resolve } = require('node:path');
 // apiScript.js     
-const API_URL = 'https://icanhazdadjoke.com/';
+// const API_URL = 'https://icanhazdadjoke.com/';
 
-const fetchJoke = () => {
-  const myObject = {
-    method: 'GET',
-    headers: { 'Accept': 'application/json' }
-  };
+// const fetchJoke = () => {
+//   const myObject = {
+//     method: 'GET',
+//     headers: { 'Accept': 'application/json' }
+//   };
 
-  fetch(API_URL, myObject)
-    .then(response => response.json())   
-    .then(data => document.getElementById('jokeContainer').innerHTML = data.joke);
+//   fetch(API_URL, myObject)
+//     .then(response => response.json())   
+//     .then(data => document.getElementById('jokeContainer').innerHTML = data.joke);
+// };
+
+// window.onload = () => fetchJoke();
+
+// ------------  A PPARTIR DO 2 ----------------
+const promise = () => {
+  const isBiggerThanEigthk = new Promise((resolve, reject) => {
+    let array = [];
+    for (let index = 0; index < 10; index += 1) {
+      let sqrNum = 0;
+      sqrNum = ((Math.random() * (50 - 1) + 1) ** 2)
+      array.push(sqrNum)
+    }
+    
+    let sum = 0;  
+    array.forEach((num) => sum += num)
+    
+    if (sum < 8000) {
+      resolve(sum);
+    } else {
+      reject(sum);
+    }
+  })
+
+  isBiggerThanEigthk
+    .then((sum) => console.log(`Promise resolvida ${sum}`))
+    .catch((sum) => console.log(`Promise rejeitada, ${sum}`))
 };
 
-window.onload = () => fetchJoke();
+promise();
