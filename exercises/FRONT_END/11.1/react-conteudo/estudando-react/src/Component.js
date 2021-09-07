a6b603fc095e31b8af35245dda9ec3a915e99fcb
Gustavo Mendes <gusm217@gmail.com>
2021-09-07 20:30:48 -0300

    Test commit

---
+++ exercises/FRONT_END/11.1/react-conteudo/estudando-react/src/Component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class primeiroComponent extends Component {
+  render() {
+    return <div>
+      <h1>Gustavo Mendes Moreira</h1>
+      <span>Meus primeiros passos com React rumo ao meu empreguinho s2</span>           
+    </div>
+  }
+}
+
+export default primeiroComponent;
