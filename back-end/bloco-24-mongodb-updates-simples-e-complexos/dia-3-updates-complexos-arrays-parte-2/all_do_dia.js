a4bb8857f581adc8a81267a859c9a29bbfa29eda
Gustavo Mendes <gusm217@gmail.com>
2021-09-16 12:07:42 -0300

    Adiciona ate o 23, com exceçao do 8. Nao consegui

--- back-end/bloco-24-mongodb-updates-simples-e-complexos/dia-3-updates-complexos-arrays-parte-2/all_do_dia.js
+++ back-end/bloco-24-mongodb-updates-simples-e-complexos/dia-3-updates-complexos-arrays-parte-2/all_do_dia.js
@@ -56,9 +56,11 @@ db.newClass.find(
 
 // 8 - Retorne todos os filmes com ratings entre 64 e 105 e divisíveis por 9 , exibindo apenas os campos title e ratings .
 // db.newClass.find(
-//   {ratings: {
-//     $all: [{ $gt: 64 }, { $lt: 105 }, {$mod: [9, 0]}] 
-//   }},
+//   {$and: [
+//     {ratings: {$gt: 64}},
+//     {ratings: {$lt: 105}},
+//     {ratings: {$mod: [9, 0]}}
+//   ]},
 //   {title: 1, ratings: 1, _id: 0}
 // );
 
@@ -87,3 +89,82 @@ db.newClass.find(
     {budget: {$mod: [5,0]}}
   ]}  
 );
+
+// 13 - Retorne os filmes da categoria "sci-fi" ou que possua o ratings maior do que 199 , exibindo apenas os campos title , ratings e category .
+db.newClass.find(
+  { $or: [{category: "sci-fi"}, {ratings: {$gt: 199}}]},
+  {title: 1, ratings: 1, category: 1, _id: 0} 
+);
+
+// 14 - Retorne os filmes em que o ratings possua tamanho 4 e que seja da category "adventure" ou "family" , mas que não tenha o imdbRating menor que 7.
+db.newClass.find(
+  { $and: [
+    {ratings: {$size: 4}},
+    {category: {$in: ["adventure", "family"]}},
+    {imdbRating: {$gt: 7}}
+  ]},  
+);
+
+// 15 - Adicione o campo description no filme Batman com o seguinte valor: "The Dark Knight of Gotham City begins his war on crime with his first major enemy being Jack Napier, a criminal who becomes the clownishly homicidal Joker."
+db.newClass.updateOne(
+  {title: "Batman"},
+  {$set: {
+    description: "The Dark Knight of Gotham City begins his war on crime with his first major enemy being Jack Napier, a criminal who becomes the clownishly homicidal Joker."
+    }
+  } 
+);
+
+// 16 - Adicione o campo description no filme Godzilla com o seguinte valor: "The world is beset by the appearance of monstrous creatures, but one of them may be the only one who can save humanity." .
+db.newClass.updateOne(
+  {title: "Godzilla"},
+  {$set: {
+    description: "The world is beset by the appearance of monstrous creatures, but one of them may be the only one who can save humanity."
+    }
+  } 
+);
+
+// 17 - Adicione o campo description no filme Home Alone com o seguinte valor: "An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation." .
+db.newClass.updateOne(
+  {title: "Home Alone"},
+  {$set: {
+    description: "An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation." 
+    }
+  } 
+);
+
+// 18 - Utilizando o operador $regex , retorne todos os filmes em que a descrição comece com a palavra "The" .
+db.newClass.find(
+  {description: {$regex: /^The/}}
+);
+
+// 19 - Utilizando o operador $regex , retorne todos os filmes em que a descrição termine com a palavra "humanity." .
+db.newClass.find(
+  {description: {$regex: /humanity.$/}}
+);
+
+// 20 - Crie um índice do tipo text no campo description .
+db.newClass.createIndex(
+  {description: "text"}
+);
+
+// 21 - Utilizando o operador $text , busque por filmes que contenham o termo "vacation" .
+db.newClass.find(
+  {$text: {
+    $search: "vacation"
+  }}
+);
+
+// 22 - Utilizando o operador $text , busque por filmes que contenham os termos "monstrous" ou "criminal" .
+db.newClass.find(
+  {$text: {
+    $search: "monstrous criminal"
+  }}
+);
+
+// 23 - Utilizando o operador $text , busque por filmes que contenham a frase "when he is accidentally" .
+db.newClass.find(
+  {$text: {
+    $search: "\"when he is accidentally\""
+  }}
+);
+
