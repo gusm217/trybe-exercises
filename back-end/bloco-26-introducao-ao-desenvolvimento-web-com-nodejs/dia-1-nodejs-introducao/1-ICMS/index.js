a4b3349b3ca6023f855fb9a03873afc0a3885531
Gustavo Mendes <gusm217@gmail.com>
2021-09-23 20:41:41 -0300

    Adiciona ex 1 com calculo de IMC

---
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/1-ICMS/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+function imcCalc() {
+  const altura = 1.72
+  const peso = 86
+  const IMC = (peso / altura ** 2 )   
+  
+  switch(true){
+    case IMC < 18.5:
+      console.log("Abaixo do peso (magreza)")
+      break
+      case IMC > 18.5 && IMC < 24.9:
+      console.log("Peso normal   ")
+      break
+      case IMC > 25 && IMC < 29.9:
+      console.log("Acima do peso (sobrepeso)")
+      break
+      case IMC > 30 && IMC < 34.9:
+      console.log("Obesidade grau I")
+      break
+      case IMC > 35 && IMC < 39.9:
+      console.log("Obesidade grau II")
+      break
+      case IMC > 40:
+      console.log("Obesidade graus III e IV")
+      break
+      default:
+      console.log("sei nem fazer um switch case");
+  }
+}
+
+imcCalc();
